aa28907187b50273fb0801d3b35b028122c597b7
Noman Shaikh <noman.shaikh@neosoftmail.com>
2023-07-24 13:10:42 +0530

    Search Integrated

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -18,10 +18,9 @@ const PromptCardList = ({ data, handleTagClick }) => {
 
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchResults, setSearchResults] = useState([]);
   const [posts, setPosts] = useState([]);
-  const handleSearch = (e) => {
-    console.log("searching");
-  };
   useEffect(() => {
     const fetchPost = async () => {
       const response = await fetch("/api/prompt");
@@ -30,6 +29,36 @@ const Feed = () => {
     };
     fetchPost();
   }, []);
+
+  const filterPrompts = (searchText) => {
+    console.log("searchText", searchText);
+    const regex = new RegExp(searchText, "i");
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearch = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        console.log("searchResult", searchResult);
+        setSearchResults(searchResult);
+      }, 4000)
+    );
+  };
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+    const searchResult = filterPrompts(tagName);
+    console.log("searchResult", searchResult);
+    setSearchResults(searchResult);
+  };
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -42,7 +71,11 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      {searchText ? (
+        <PromptCardList data={searchResults} handleTagClick={handleTagClick} />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
